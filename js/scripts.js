784c72cdfffbed5fecd0c468d992f4e0b407b126
Ben Pritchard and Duane Moody <dmmoody@gmail.com>
2015-02-17 12:46:55 -0800

    Add jquery and html to display triangle type on webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var triangles = function(side1, side2, side3) {
-  var sides = [side1, side2, side3].sort()
+  side1 = parseInt(side1);
+  side2 = parseInt(side2);
+  side3 = parseInt(side3);
+  var sides = [side1, side2, side3].sort();
+  
   if (sides[0] + sides[1] <= sides[2]) {
     return false;
   } else if ((side1 === side2) && (side2 === side3)) {
@@ -10,3 +14,29 @@ var triangles = function(side1, side2, side3) {
     return 'scalene';
   }
 };
+
+$(document).ready(function() {
+  $("#sides").submit(function(event) {
+    event.preventDefault();
+
+    var side1 = $("#side1").val();
+    var side2 = $("#side2").val();
+    var side3 = $("#side3").val();
+
+    var type = triangles(side1, side2, side3);
+    // var showSides = function() {
+    //   return side1 + ", " + side2 + ", " + side3;
+    // }
+
+    if (type === false) {
+      alert("These side lengths do not form a triangle!");
+    } else if (type === "equilateral") {
+      $(".equilateral").append("<li>" + side1 + ", " + side2 + ", " + side3 + "</li>");
+    } else if (type === "isosceles") {
+      $(".isosceles").append("<li>" + side1 + ", " + side2 + ", " + side3 + "</li>");
+    } else {
+      $(".scalene").append("<li>" + side1 + ", " + side2 + ", " + side3 + "</li>");
+    }
+
+  });
+});
